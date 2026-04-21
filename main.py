8eaffb341c926a4825e650f4b80159adfffe3cab
Ataxanov <asherzod2197@gmail.com>
2026-04-21 20:13:49 +0500

    Add FoodOrder class with delivery functionality

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+class FoodOrder:
+    def __init__(self, food, price, is_delivered=False):
+        self.food = food
+        self.price = price
+        self.is_delivered = is_delivered
 
+    def deliver(self):
+        self.is_delivered = True
+        print("Buyurtma yetkazildi")
+
+    def status(self):
+        if self.is_delivered:
+            print("Buyurtma yetkazildi")
+        else:
+            print("Hali yetkazilmadi")
+
+
+# Obyekt
+order = FoodOrder("Lavash", 25000)
+order.status()
+order.deliver()
+order.status()
